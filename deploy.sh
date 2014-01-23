62f8ebd483cdcdb459aa32bb7e69b0d0416b5fe4
Tobias Kahan <tobyk100@gmail.com>
2014-01-23 10:19:00 -0800

    Wrap debug and normal build into deploy which takes a single argument

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-chmod +x ../closure-library-read-only/closure/bin/build/closurebuilder.py
-../closure-library-read-only/closure/bin/build/closurebuilder.py \
---root=../closure-library-read-only/ --root=core/ \
---compiler_jar=compiler.jar --namespace="Blockly" --output_mode=compiled \
-> blockly_compressed.js
+if [[ $1 == "debug" ]]; then
+  ../closure-library-read-only/closure/bin/build/closurebuilder.py \
+  --root=../closure-library-read-only/ --root=core/ \
+  --compiler_jar=compiler.jar --compiler_flags="--compilation_level=WHITESPACE_ONLY" \
+  --compiler_flags="--formatting=PRETTY_PRINT" \
+  --namespace="Blockly" --output_mode=compiled \
+  > blockly_debug.js
+else
+  chmod +x ../closure-library-read-only/closure/bin/build/closurebuilder.py
+  ../closure-library-read-only/closure/bin/build/closurebuilder.py \
+  --root=../closure-library-read-only/ --root=core/ \
+  --compiler_jar=compiler.jar --namespace="Blockly" --output_mode=compiled \
+  > blockly_compressed.js
+fi
