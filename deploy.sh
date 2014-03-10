fee4469c0fb91784c2dbf81a1a7a18047503ae92
Geoffrey Elliott <geoffrey@code.org>
2014-03-10 18:03:47 +0000

    Updated deploy script to call python instead of requiring a change to the repo

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 if [[ $1 == "debug" ]]; then
-  ../closure-library-read-only/closure/bin/build/closurebuilder.py \
+  python ../closure-library-read-only/closure/bin/build/closurebuilder.py \
   --root=../closure-library-read-only/ --root=core/ \
   --compiler_jar=compiler.jar --compiler_flags="--compilation_level=WHITESPACE_ONLY" \
   --compiler_flags="--formatting=PRETTY_PRINT" \
   --namespace="Blockly" --output_mode=compiled \
   > blockly_debug.js
 else
-  chmod +x ../closure-library-read-only/closure/bin/build/closurebuilder.py
-  ../closure-library-read-only/closure/bin/build/closurebuilder.py \
+  python ../closure-library-read-only/closure/bin/build/closurebuilder.py \
   --root=../closure-library-read-only/ --root=core/ \
   --compiler_jar=compiler.jar --namespace="Blockly" --output_mode=compiled \
   > blockly_compressed.js
